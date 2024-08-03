db52160ad4a9b6459bca957ab02a272af8f5312c
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-08-03 22:47:37 +0530

    added update task route

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -23,3 +23,42 @@ export const createTask = async (req, res, next) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const getAllTask = async (req, res, next) => {
+  try {
+    const task_list = await TodoModel.find();
+    return res.status(200).json({ message: "All tasks", task_List: task_list });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateTask = async (req, res, next) => {
+  try {
+    const { task, isCompleted } = req.body;
+
+    const taskId = req.params.id;
+
+    if (!taskId) {
+      return res.status(400).json({ message: "Task ID is required" });
+    }
+
+    const updatedTask = await TodoModel.findByIdAndUpdate(
+      taskId,
+      { task, isCompleted },
+      { new: true }
+    );
+
+    if (!updatedTask) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Task updated successfully", task: updatedTask });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- routes/todo.route.js
+++ routes/todo.route.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { createTask } from "../controllers/todo.controller.js";
+import {
+  createTask,
+  getAllTask,
+  updateTask,
+} from "../controllers/todo.controller.js";
 
 export const todoRouter = express.Router();
 
 todoRouter.post("/create_task", createTask);
-todoRouter.get("/getall_task");
+todoRouter.get("/getall_task", getAllTask);
 todoRouter.get("/get_task/:id");
-todoRouter.patch("/update_task/:id");
+todoRouter.patch("/update_task/:id", updateTask);
 todoRouter.delete("/delete_task?:id");
